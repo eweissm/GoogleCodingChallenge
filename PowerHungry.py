3a1fe6abb42788e026e49dc96e0c9191aee37e33
Ericw <eweissm1@asu.edu>
2023-12-20 11:56:56 -0700

    new commit

--- PowerHungry.py
+++ PowerHungry.py
@@ -5,26 +5,26 @@
 # if we have an even num of neg values --> add the negative values back to the list
 # else, we will remove smallest magnitude neg and then
 
-    def solution(xs):
-        areZeros = 0 in xs
-        negativeValues = [i for i in xs if i < 0]
-        xs = [i for i in xs if i > 0]
+def solution(xs):
+    areZeros = 0 in xs
+    negativeValues = [i for i in xs if i < 0]
+    xs = [i for i in xs if i > 0]
 
-        if len(negativeValues) % 2 != 0 and (bool(xs) or areZeros):
-            negativeValues.sort()
-            del negativeValues[-1]
-        xs = xs + negativeValues
+    if len(negativeValues) % 2 != 0 and (bool(xs) or areZeros):
+        negativeValues.sort()
+        del negativeValues[-1]
+    xs = xs + negativeValues
 
-        if bool(xs):
-            prod = 1
-            for j in range(len(xs)):
-                prod = prod * xs[j]
-        else:
-            prod = 0
-        return str(prod)
+    if bool(xs):
+        prod = 1
+        for j in range(len(xs)):
+            prod = prod * xs[j]
+    else:
+        prod = 0
+    return str(prod)
 
-    print(solution([-8, 2]))  # 2
-    print(solution([2, 0, 2, 2, 0]))  # 8
-    print(solution([-2, -3, 4, -5]))  # 60
-    print(solution([0, 0, 0, 0, -1]))  # 0
-    print(solution([-4]))  # -4
+print(solution([-8, 2]))  # 2
+print(solution([2, 0, 2, 2, 0]))  # 8
+print(solution([-2, -3, 4, -5]))  # 60
+print(solution([0, 0, 0, 0, -1]))  # 0
+print(solution([-4]))  # -4
