d8143ee30207a133729e7073c3a06c350c98f7de
Ericw <eweissm1@asu.edu>
2023-12-19 22:20:23 -0700

    new commit

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 ## challenge 2
 
 # take a list of integers and find the subset which returns the highest product
+#  remove integers that will be useless for now (i.e. 0) ... hold on to the negatives on the side
+# if we have an even num of neg values --> add the negative values back to the list
+#else, we will remove smallest magnitude neg and then
+
 
 import math
-
 def solution(xs):
-
+    xs.sort()
     negativeValues = []
 
-#remove integers that will be useless for now (i.e. 0) ... hold on to the negatives on the side
     for i in range(len(xs)):
-        if xs[i]==0:
-            del xs[i]
-        elif xs[i]<0:
+        if xs[i] == 0:
+            xs[i] = 1
+        elif xs[i] < 0:
             negativeValues.append(xs[i])
-            del xs[i]
+            xs[i] = 1
 
-    if len(negativeValues) % 2 == 0:  # if we have an even num of neg values --> add the negative values back to the list
-        xs.append(negativeValues)
-    elif len(negativeValues)>0:
-        negativeValues.sort()
-        del negativeValues[0]
-        xs.append(negativeValues)
+    if len(negativeValues) % 2 != 0:
+        del negativeValues[-1]
+
+    xs = xs+negativeValues
 
     return str(math.prod(xs))
+
+print(solution([-2, -3, 4, -5]))
