101d34f77372ffc62e11faee6dc5f6dd37dd2b84
Ericw <eweissm1@asu.edu>
2023-12-20 10:26:23 -0700

    new commit

--- main.py
+++ main.py
@@ -6,9 +6,10 @@
 # else, we will remove smallest magnitude neg and then
 
 def solution(xs):
+    areZeros = 0 in xs
     negativeValues = [i for i in xs if i < 0]
     xs = [i for i in xs if i > 0]
-    if len(negativeValues) % 2 != 0 and bool(xs):
+    if len(negativeValues) % 2 != 0 and (bool(xs) or areZeros):
         negativeValues.sort()
         del negativeValues[-1]
     xs = xs+negativeValues
@@ -24,5 +25,5 @@ def solution(xs):
 print(solution([-8,  2])) #2
 print(solution([2, 0, 2, 2, 0])) #8
 print(solution([-2, -3, 4, -5])) #60
-print(solution([0,0,0,0])) #0
+print(solution([0,0,0,0, -1])) #0
 print(solution([-4])) #-4
