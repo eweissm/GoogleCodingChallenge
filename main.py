d27af442f390f9d03c2c948fe401f6aa6873376d
Ericw <eweissm1@asu.edu>
2023-12-19 21:40:58 -0700

    new commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 def solution(s):
     brailleDictionary = {
-        "a": 32, "b": 48, "c": 36, "d": 38, "e": 34, "f": 52, "g": 54, "h": 50, "i": 20, "j": 22,
+        " ": 0, "a": 32, "b": 48, "c": 36, "d": 38, "e": 34, "f": 52, "g": 54, "h": 50, "i": 20, "j": 22,
         "k": 40, "l": 56, "m": 44, "n": 46, "o": 42, "p": 60, "q": 62, "r": 58, "s": 28, "t": 30, "u": 41,
         "v": 57, "w": 23, "x": 45, "y": 47, "z": 43}
 
@@ -13,10 +13,5 @@ def solution(s):
     for x in s:
         if x.isupper():
             OutputStr = OutputStr + "000001"
-        elif x == " ":
-            OutputStr = OutputStr + "000000"
-            continue
-        OutputStr=OutputStr + str(format(brailleDictionary[x.lower()],'b'))
+        OutputStr=OutputStr + format(brailleDictionary[x.lower()],'06b')
     return OutputStr
-
-print(solution("code"))
